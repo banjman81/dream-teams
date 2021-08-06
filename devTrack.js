8945357b89b6d949c35651bbad1b5b91be79c70d
[banjman81] <[benjamin.man97@gmail.com]>
2021-08-06 13:31:17 -0500

    third commit

--- devTrack.js
+++ devTrack.js
@@ -1,7 +1,7 @@
-async function loadDevTracK(){
-    const rawData = await fetch('https://newworldfans.com/api/v1/dev_tracker?page=1&source=reddit')
-    const data = await rawData.json()
-    console.log(data.dev_posts)
-}
+// async function loadDevTracK(){
+//     const rawData = await fetch('https://newworldfans.com/api/v1/dev_tracker?page=1&source=reddit')
+//     const data = await rawData.json()
+//     console.log(data.dev_posts)
+// }
 
-loadDevTracK()
+// loadDevTracK()
